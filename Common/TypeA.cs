afa6f8e7a92f4e1f607830c1cc63eb475a9adc02
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-12 16:07:47 +0200

    Protected & Private Protected & Internal Protected

---
+++ Common/TypeA.cs
@@ -0,0 +1,9 @@
+﻿namespace Common
+{
+    public class TypeA
+    {
+        protected internal int Z;
+        protected int Y;
+        private protected int X;
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Demo
+﻿using TypeB = Common.TypeB;
+using TypeBB0 = Demo.New.TypeB;
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
