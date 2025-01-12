a1b7b9f2754fc059f78684a77964b5b7fe7c0d6f
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-12 16:25:03 +0200

    Polymorphism[Overloading]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,9 +4,28 @@ namespace Demo
 {
     internal class Program
     {
+        #region Polymorphism[Overloading]
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Sum(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Sum(int x, double y)
+        {
+            return x + y;
+        } 
+        #endregion
+
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
